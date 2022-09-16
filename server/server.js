ba9b2177d39edcd4b299693cc2fdf64a788a9e2c
risa-10 <106301648+risa-10@users.noreply.github.com>
2022-09-15 17:40:22 -0700

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// uncomment the below for proxy challenge
+const leaderList = [
+  {name: 'Anna', id: 'a0'},
+  {name: 'Ben', id: 'b0'},
+  {name: 'Clara', id: 'c0'},
+  {name: 'David', id: 'd0'},
+];
+
+app.get('/api/leaders', (req, res) => {
+  return res.status(200).send(leaderList);
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+  });
+};
+
+app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.jsx?/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        },
+        {
+            test: /\.(s?(a|c)ss)$/i,
+            use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader",
+            ]
+        }
+      ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+      publicPath: '/build'
+    },
+    proxy: {
+      '/api': 'http://localhost:3000'
+    },
+  },
+}
